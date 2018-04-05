73ee78222dc766d9552f3ec1d9c89d4e0be7886d
zhang21 <elite_zhang21@163.com>
2018-04-05 11:29:48 +0800

    Python基础

---
+++ chap01/hello.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+# coding: utf-8
+
+print('Hello world!')
+print('what is your name?')
+
+name = input()
+print('很高兴认识你,' + name)
+print('the length of your name is:')
+print(len(name))
+
+print('你多大了？')
+age = input()
+print('you will be ' + str(int(age) + 1) + ' in a year.')
+
